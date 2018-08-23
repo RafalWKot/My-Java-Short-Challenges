b4a3bd97dd218c84c7fd5312670a3a14f9e83663
RafalWKot <rafal.kot.poczta@gmail.com>
2018-08-23 07:59:19 +0200

    Add challenge "StringWithoutX2" from www.codingbat.com

---
+++ src/main/java/StringWithoutX2.java
@@ -0,0 +1,17 @@
+// Given a string, if one or both of the first 2 chars is 'x', return the string without those 'x' chars,
+// and otherwise return the string unchanged. This is a little harder than it looks.
+//        withoutX2("xHi") → "Hi"
+//        withoutX2("Hxi") → "Hi"
+//        withoutX2("Hi") → "Hi"
+
+public class StringWithoutX2 {
+    public String getStringWithoutX2(String str) {
+        StringBuilder newStr = new StringBuilder();
+        for (int j = 0; j < str.length(); j++) {
+            if (!((j == 0 || j == 1) && (str.charAt(j) == 'x'))) {
+                newStr.append(str.charAt(j));
+            }
+        }
+        return newStr.toString();
+    }
+}
---
+++ src/test/java/StringWithoutX2Test.java
@@ -0,0 +1,44 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringWithoutX2Test {
+
+    private StringWithoutX2 theString;
+
+    @Before
+    public void setUp() throws Exception {
+        theString = new StringWithoutX2();
+    }
+
+    @Test
+    public void stringFirstPlace() {
+        //Given
+        String str = "xHart";
+        //When
+        str = theString.getStringWithoutX2(str);
+        //Then
+        assertEquals("Hart", str);
+    }
+
+    @Test
+    public void stringSecondPlace() {
+        //Given
+        String str = "Hxart";
+        //When
+        str = theString.getStringWithoutX2(str);
+        //Then
+        assertEquals("Hart", str);
+    }
+
+    @Test
+    public void stringManyX() {
+        //Given
+        String str = "xxHarxxt";
+        //When
+        str = theString.getStringWithoutX2(str);
+        //Then
+        assertEquals("Harxxt", str);
+    }
+}
