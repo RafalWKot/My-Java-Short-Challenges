6f394351f891a69d26f15f3df79d84a8794d3d3a
RafalWKot <rafal.kot.poczta@gmail.com>
2019-01-22 22:00:55 +0100

    Implement challenge "PersistentBugger".

--- src/main/java/PersistentBugger/Persist.java
+++ src/main/java/PersistentBugger/Persist.java
@@ -1,8 +1,28 @@
 package PersistentBugger;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Persist {
     public static int persistence(long n) {
 
-        return -1;
+        int counter = 0;
+        while (n > 9) {
+
+            List<Long> numbers = Arrays.stream(String.valueOf(n).split(""))
+                    .map(Long::valueOf)
+                    .collect(Collectors.toList());
+
+            n = 1;
+
+            for (int i = 0; i < numbers.size(); i++) {
+                n *=  numbers.get(i);
+            }
+
+            counter++;
+        }
+
+        return counter;
     }
 }
