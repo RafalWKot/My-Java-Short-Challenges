01db15cbbc24056ee5e4b46c5a255d606cfd81db
RafalWKot <rafal.kot.poczta@gmail.com>
2019-02-21 18:48:24 +0100

    Add implementation of task  "Highest Scoring Word".

--- src/main/java/HighestScoringWord/HighestScoringWord.java
+++ src/main/java/HighestScoringWord/HighestScoringWord.java
@@ -1,8 +1,27 @@
 package HighestScoringWord;
 
 public class HighestScoringWord {
+    private static final int ASCII_START_VALUE = 96;
+
     public static String high(String s) {
-        // Your code here...
-        return "";
+
+        String [] words = s.split(" ");
+
+        String highestScoreWord = "";
+        int highestScore = 0;
+
+        int actualScore = 0;
+        for (String word: words) {
+            for (char ch: word.toCharArray()) {
+                actualScore += (int)ch -  ASCII_START_VALUE;
+            }
+            if (actualScore > highestScore) {
+                highestScoreWord = word;
+                highestScore = actualScore;
+            }
+            actualScore = 0;
+        }
+
+        return highestScoreWord;
     }
 }
