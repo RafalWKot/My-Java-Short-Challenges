dc1edba23c2fa5aed63c2814367b093e301ccbd3
RafalWKot <rafal.kot.poczta@gmail.com>
2019-02-27 08:52:32 +0100

    Add implementation to "Directions Reduction".

--- src/main/java/DirectionsReduction/DirectionsReduction.java
+++ src/main/java/DirectionsReduction/DirectionsReduction.java
@@ -4,6 +4,44 @@ public class DirectionsReduction {
 
     public static String[] dirReduc(String[] array) {
 
-        return null;
+        String[] reductionedDirection = new String[]{"NORTHSOUTH", "SOUTHNORTH", "EASTWEST", "WESTEAST"};
+        String[] arrayToChange = array;
+        boolean endCondition = true;
+
+        while (endCondition) {
+            endCondition = false;
+            for (int i = 0; i < arrayToChange.length - 1; i++) {
+                if (ifContain(arrayToChange[i], arrayToChange[i + 1], reductionedDirection)) {
+                    arrayToChange = createNewArray(i, i + 1, arrayToChange);
+                    endCondition = true;
+                    break;
+                }
+            }
+        }
+
+        return arrayToChange;
+    }
+
+    private static boolean ifContain(String firstDirection, String secondDirection, String[] arrayOfReductionDirection) {
+
+        for (String direction : arrayOfReductionDirection) {
+            if (direction.equals(firstDirection + secondDirection)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static String[] createNewArray(int firstIndexOut, int secondIndexOut, String[] arrayToChange) {
+
+        String[] newArray = new String[arrayToChange.length - 2];
+        int newArrayIndex = 0;
+        for (int i = 0; i < arrayToChange.length; i++) {
+            if (i != firstIndexOut && i != secondIndexOut) {
+                newArray[newArrayIndex] = arrayToChange[i];
+                newArrayIndex++;
+            }
+        }
+        return newArray;
     }
 }
